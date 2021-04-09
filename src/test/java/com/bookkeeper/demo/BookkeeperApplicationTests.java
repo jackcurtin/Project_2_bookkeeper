6d43b09a2b8c38ce9218c7ae7abbfbfaf208fd41
Jack Curtin <jackcurtin94@gmail.com>
2021-04-09 12:06:44 -0500

    created project file

---
+++ src/test/java/com/bookkeeper/demo/BookkeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookkeeper.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookkeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

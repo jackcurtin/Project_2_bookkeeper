4cb8b7e90848920025ed3dd142fb51e5cec10cb9
Jack Curtin <jackcurtin94@gmail.com>
2021-04-11 13:25:55 -0500

    basic get function for bookController and Service

--- src/main/java/com/bookkeeper/demo/controller/BookController.java
+++ src/main/java/com/bookkeeper/demo/controller/BookController.java
@@ -1,4 +1,29 @@
 package com.bookkeeper.demo.controller;
 
+import com.bookkeeper.demo.model.Book;
+import com.bookkeeper.demo.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path="/api")
 public class BookController {
+
+    private BookService bookService;
+
+    @Autowired
+    public void setBookService(BookService bookService){
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/books")
+    public List<Book> getAllBooks(){
+        System.out.println("Calling getAllBooks");
+        return bookService.getAllBooks();
+    }
+
 }
--- src/main/java/com/bookkeeper/demo/service/BookService.java
+++ src/main/java/com/bookkeeper/demo/service/BookService.java
@@ -1,4 +1,28 @@
 package com.bookkeeper.demo.service;
 
+import com.bookkeeper.demo.exception.InformationExistsException;
+import com.bookkeeper.demo.model.Book;
+import com.bookkeeper.demo.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BookService {
+    private BookRepository bookRepository;
+
+    @Autowired
+    public void setBookRepository (BookRepository bookRepository){
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> getAllBooks(){
+        System.out.println("Service is calling getAllBooks");
+        List<Book> allBooks = bookRepository.findAll();
+        if(allBooks.isEmpty()){
+            throw new InformationExistsException("there are no books here");
+        }
+        return allBooks;
+    }
 }

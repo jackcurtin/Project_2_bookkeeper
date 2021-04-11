bb3bc6fea1974d72299fe84a37227651570125c1
Bharti gupta <bhartigupta1587@gmail.com>
2021-04-11 17:37:48 -0500

    all crud Operation on book

--- src/main/java/com/bookkeeper/demo/controller/BookController.java
+++ src/main/java/com/bookkeeper/demo/controller/BookController.java
@@ -1,4 +1,62 @@
 package com.bookkeeper.demo.controller;
 
+import com.bookkeeper.demo.model.Book;
+import com.bookkeeper.demo.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
 public class BookController {
+
+    private BookService bookService;
+
+    @Autowired
+    public void setBookService(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    //http://localhost:9091/api/hello
+    @RequestMapping("/hello")
+    public String helloWorld() {
+        return "Hello world";
+    }
+
+    //http://localhost:9091/api/books
+    @PostMapping("/books")
+    public Book createBook(@RequestBody Book bookObject) {
+        System.out.println("calling from createBook");
+        return bookService.createBook(bookObject);
+    }
+
+    //http://localhost:9091/api/books
+    @GetMapping("/books/{bookId}")
+    public Optional getBook(@PathVariable Long bookId) {
+        System.out.println("calling createBook");
+        return bookService.getBook(bookId);
+    }
+
+    //http://localhost:9091/api/books
+    @GetMapping("/books")
+    public List<Book> getBooks() {
+        System.out.println("calling getBooks");
+        return bookService.getBooks();
+    }
+
+    //http://localhost:9091/api/books/1
+    @PutMapping("/books/{bookId}")
+    public Book updateBook(@PathVariable Long bookId, @RequestBody Book bookObject) {
+        System.out.println("calling fon UpdateBook");
+        return bookService.updateBook(bookId, bookObject);
+    }
+
+    //http://localhost:9091/api/books/1
+    @DeleteMapping("/books/{bookId}")
+    public String deleteBook(@PathVariable Long bookId) {
+        System.out.println("calling from DeleteBook");
+        return bookService.deleteBook(bookId);
+    }
 }
--- src/main/java/com/bookkeeper/demo/service/BookService.java
+++ src/main/java/com/bookkeeper/demo/service/BookService.java
@@ -1,4 +1,76 @@
 package com.bookkeeper.demo.service;
 
+import com.bookkeeper.demo.exception.InformationExistsException;
+import com.bookkeeper.demo.exception.InformationNotFoundException;
+import com.bookkeeper.demo.model.Book;
+import com.bookkeeper.demo.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class BookService {
+    private BookRepository bookRepository;
+
+    @Autowired
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public Book createBook(Book bookObject) {
+        System.out.println("Service Calling CreateBook");
+        Book book = bookRepository.findByTitle(bookObject.getTitle());
+        if (book == null) {
+            return bookRepository.save(bookObject);
+        } else {
+            throw new InformationExistsException("Book with this title " + bookObject.getTitle() + "already exist");
+        }
+    }
+
+    public Optional getBook(Long bookId) {
+        System.out.println("service Calling createBook");
+        Optional book = bookRepository.findById(bookId);
+        if (book.isPresent()) {
+            return book;
+        } else {
+            throw new InformationNotFoundException("Book with id" + bookId + "not found");
+        }
+    }
+
+    public List<Book> getBooks() {
+        System.out.println("Service calling getBooks");
+        return bookRepository.findAll();
+    }
+
+    public Book updateBook(Long bookId, Book bookObject) {
+        System.out.println("service calling update Book");
+        Optional<Book> book = bookRepository.findById(bookId);
+        if (book.isPresent()) {
+            if (bookObject.getIsbn() == (book.get().getIsbn())) {
+                System.out.println("same book");
+                throw new InformationExistsException("book of this " + book.get().getTitle() + "already exist");
+            } else {
+                bookObject.setTitle(bookObject.getTitle());
+                bookObject.setSynopsis(bookObject.getSynopsis());
+                bookObject.setPageCount(bookObject.getPageCount());
+                return bookRepository.save(bookObject);
+            }
+        } else {
+            throw new InformationNotFoundException("Book with id " + bookId + "not found");
+        }
+    }
+
+    public String deleteBook(Long bookId) {
+        System.out.println("service calling deleteBook");
+        Optional<Book> book = bookRepository.findById(bookId);
+        if ( book.isPresent()) {
+            bookRepository.deleteById(bookId);
+            return "book " + bookId + " deleted";
+        } else {
+            throw new InformationNotFoundException("Book with id " + bookId + "not found");
+        }
+    }
+
 }

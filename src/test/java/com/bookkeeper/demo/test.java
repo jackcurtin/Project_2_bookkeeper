df1a7663a0d245213cf0c0e3ea95c3cf97eba1b7
Jack Curtin <jackcurtin94@gmail.com>
2021-04-15 09:43:09 -0500

    added test for addBook... does not work yet

--- src/test/java/com/bookkeeper/demo/test.java
+++ src/test/java/com/bookkeeper/demo/test.java
@@ -8,7 +8,10 @@ import com.bookkeeper.demo.repository.AuthorRepository;
 import com.bookkeeper.demo.repository.BookRepository;
 import com.bookkeeper.demo.repository.GenreRepository;
 import com.bookkeeper.demo.repository.PublisherRepository;
+import com.bookkeeper.demo.service.AuthorService;
 import com.bookkeeper.demo.service.BookService;
+import com.bookkeeper.demo.service.GenreService;
+import com.bookkeeper.demo.service.PublisherService;
 import org.hamcrest.collection.IsMapContaining;
 
 
@@ -50,6 +53,15 @@ public class test {
     @InjectMocks
     BookService bookService;
 
+    @InjectMocks
+    GenreService genreService;
+
+    @InjectMocks
+    AuthorService authorService;
+
+    @InjectMocks
+    PublisherService publisherService;
+
     @Test
     public void testFindAllBooks(){
         List<Book> books = new ArrayList<Book>();
@@ -60,34 +72,35 @@ public class test {
         assertEquals(3, bookService.getAllBooks().size());
     }
 
-    @Test
-    public void testAddBook(){
-        Map <String, String> bookObject = new HashMap<String, String>();
-        bookObject.put("title", "bookTitle");
-        bookObject.put("synopsis", "very large");
-        bookObject.put("pageCount", "40000");
-        bookObject.put("isbn", "10002349902");
-        bookObject.put("genre_name", "history");
-        bookObject.put("author_first_name", "William");
-        bookObject.put("author_last_name","Gibson");
-        bookObject.put("publisher_name","Reading Place");
-        Genre genre = new Genre("history", "happened");
-        Author author = new Author("William", "Gibson", 70, "usa");
-        Publisher publisher = new Publisher("Reading Place", "123 Main St");
-        genreRepositoryMock.save(genre);
-        authorRepositoryMock.save(author);
-        publisherRepositoryMock.save(publisher);
-
-        assertThat(bookObject.size(), is(8));
-        assertThat(bookObject, not(IsMapContaining.hasEntry("age", "Gibson")));
-        assertThat(bookObject, IsMapContaining.hasKey("isbn"));
-        when(bookRepositoryMock.findByTitleIgnoreCase(bookObject.get("title"))).thenReturn(null);
-        when(genreRepositoryMock.findByName(bookObject.get("genre_name"))).thenReturn(Optional.of(genre));
-        when(authorRepositoryMock.findByFirstNameAndLastName(bookObject.get("author_first_name"), bookObject.get("author_last_name")))
-                .thenReturn(Optional.of(author));
-        when(publisherRepositoryMock.findByName(bookObject.get("publisher_name"))).thenReturn(Optional.of(publisher));
-        Book actual = bookService.addBook(bookObject);
-        assertEquals(bookObject, IsMapContaining.hasValue(actual.getIsbn()));
-    }
-
+//    @Test
+//    public void testAddBook(){
+//        Map <String, String> bookObject = new HashMap<String, String>();
+//        bookObject.put("title", "bookTitle");
+//        bookObject.put("synopsis", "very large");
+//        bookObject.put("pageCount", "40000");
+//        bookObject.put("isbn", "10002349902");
+//        bookObject.put("genre_name", "history");
+//        bookObject.put("author_first_name", "William");
+//        bookObject.put("author_last_name","Gibson");
+//        bookObject.put("publisher_name","Reading Place");
+//        Genre genre = new Genre("history", "happened");
+//        Author author = new Author("William", "Gibson", 70, "usa");
+//        Publisher publisher = new Publisher("Reading Place", "123 Main St");
+//        genreService.addGenre(genre);
+//        authorService.addAuthor(author);
+//        publisherService.addPublisher(publisher);
+//
+//        System.out.println(genreRepositoryMock.findAll());
+//
+//        assertThat(bookObject.size(), is(8));
+//        assertThat(bookObject, not(IsMapContaining.hasEntry("age", "Gibson")));
+//        assertThat(bookObject, IsMapContaining.hasKey("isbn"));
+//        when(bookRepositoryMock.findByTitleIgnoreCase(bookObject.get("title"))).thenReturn(null);
+//        when(genreRepositoryMock.findByName(bookObject.get("genre_name"))).thenReturn(genreRepositoryMock.findByName(bookObject.get("genre_name")));
+//        when(authorRepositoryMock.findByFirstNameAndLastName(bookObject.get("author_first_name"), bookObject.get("author_last_name")))
+//                .thenReturn(Optional.of(author));
+//        when(publisherRepositoryMock.findByName(bookObject.get("publisher_name"))).thenReturn(Optional.of(publisher));
+//        Book actual = bookService.addBook(bookObject);
+//        assertEquals(bookObject, IsMapContaining.hasValue(actual.getIsbn()));
+//    }
 }

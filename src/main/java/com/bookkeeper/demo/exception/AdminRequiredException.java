1be95993028055387e7d8379b430e3ae98e8bd49
Jack Curtin <jackcurtin94@gmail.com>
2021-04-16 09:51:14 -0500

    commiting admin require exception class

--- src/main/java/com/bookkeeper/demo/exception/AdminRequiredException.java
+++
@@ -1,9 +0,0 @@
-package com.bookkeeper.demo.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.FORBIDDEN)
-public class AdminRequiredException extends RuntimeException{
-    public AdminRequiredException(String message) {super(message);}
-}
